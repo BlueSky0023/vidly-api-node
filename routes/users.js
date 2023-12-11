f21d7e6be31f3e7d4aee3dca195f1aae027b1139
BlueSky0023 <programmingwithdennis01@gmail.com>
2023-12-11 17:09:35 -0500

    index.js/registration.html

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,7 @@ router.get("/me", auth, async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
+  console.log("Received registration request"); // Add this line for logging
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 

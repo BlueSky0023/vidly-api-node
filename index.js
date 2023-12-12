6c3b6e14c9796551376bc589e2c5bcd569dd6ea4
BlueSky0023 <programmingwithdennis01@gmail.com>
2023-12-11 21:11:10 -0500

    index.js/routes/users.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const winston = require("winston");
 const express = require("express");
 const config = require("config");
+const bodyParser = require('body-parser'); // Import body-parser module
 const app = express();
 
 require("./startup/logging")();
@@ -9,6 +10,14 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.post("/", async (req, res) => {
+  console.log('Received registration request');
+  // Handle the POST request logic here
+});
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>

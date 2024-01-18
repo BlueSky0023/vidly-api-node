4de8c90cde3bd1b5f9089ef52d37d4b889e3ca77
BlueSky0023 <programmingwithdennis01@gmail.com>
2024-01-18 03:29:35 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const winston = require("winston");
 const express = require("express");
 const config = require("config");
-const bodyParser = require('body-parser'); // Import body-parser module
 const app = express();
 
 require("./startup/logging")();
@@ -10,14 +9,6 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
-
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-
-app.post("/", async (req, res) => {
-  console.log('Received registration request');
-  // Handle the POST request logic here
-});
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
